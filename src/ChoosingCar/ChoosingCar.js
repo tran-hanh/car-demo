330e2ceeb4ad9586b372271f3ef704d9b38e5e11
Hanh <tran.hanh3012@gmail.com>
2020-09-09 22:01:05 +1000

    Add car choosing function

--- src/ChoosingCar/ChoosingCar.js
+++ src/ChoosingCar/ChoosingCar.js
@@ -1,72 +1,109 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default function ChoosingCar() {
-  return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-7">
-          <img
-            style={{ width: "100%" }}
-            src={require("../assets/products/black-car.jpg")}
-            alt="black_car"
-          />
-        </div>
-        <div className="col-5">
-          <div className="card text-dark">
-            <div className="card-header">Exterior Color</div>
-            <div className="card-body">
-              <div
-                className="row border border-link pt-2 pb-2 mt-2"
-                onClick={() => {}}
-                style={{ cursor: "pointer" }}
-              >
-                <img
-                  className="col-2"
-                  src={require("../assets/icons/icon-black.jpg")}
-                  alt="black_icon"
-                />
-                <div className="col-10">
-                  <h3>Crystal Black</h3>
-                  <p>Pearl</p>
+export default class ChoosingCar extends Component {
+  state = {
+    imgProduct: require("../assets/products/black-car.jpg"),
+  };
+
+  renderCar = (imgNewProduct) => {
+    //Create a new state
+    let newState = {
+      imgProduct: imgNewProduct,
+    };
+
+    this.setState(newState);
+  };
+
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-7">
+            <img
+              style={{ width: "100%" }}
+              src={this.state.imgProduct}
+              alt="black_car"
+            />
+          </div>
+          <div className="col-5">
+            <div className="card text-dark">
+              <div className="card-header">Exterior Color</div>
+              <div className="card-body">
+                <div
+                  className="row border border-link pt-2 pb-2 mt-2"
+                  onClick={() => {
+                    this.renderCar(require("../assets/products/black-car.jpg"));
+                  }}
+                  style={{ cursor: "pointer" }}
+                >
+                  <img
+                    className="col-2"
+                    src={require("../assets/icons/icon-black.jpg")}
+                    alt="black_icon"
+                  />
+                  <div className="col-10">
+                    <h3>Crystal Black</h3>
+                    <p>Pearl</p>
+                  </div>
                 </div>
-              </div>
-              <div className="row border border-link pt-2 pb-2 mt-2">
-                <img
-                  className="col-2"
-                  src={require("../assets/icons/icon-steel.jpg")}
-                  alt="steel_icon"
-                />
-                <div className="col-10">
-                  <h3>Modern Steel</h3>
-                  <p>Metalic</p>
+                <div
+                  className="row border border-link pt-2 pb-2 mt-2"
+                  onClick={() => {
+                    this.renderCar(require("../assets/products/steel-car.jpg"));
+                  }}
+                  style={{ cursor: "pointer" }}
+                >
+                  <img
+                    className="col-2"
+                    src={require("../assets/icons/icon-steel.jpg")}
+                    alt="steel_icon"
+                  />
+                  <div className="col-10">
+                    <h3>Modern Steel</h3>
+                    <p>Metalic</p>
+                  </div>
                 </div>
-              </div>
-              <div className="row border border-link pt-2 pb-2 mt-2">
-                <img
-                  className="col-2"
-                  src={require("../assets/icons/icon-silver.jpg")}
-                  alt="silver_icon"
-                />
-                <div className="col-10">
-                  <h3>Crystal Black</h3>
-                  <p>Pearl</p>
+                <div
+                  className="row border border-link pt-2 pb-2 mt-2"
+                  onClick={() => {
+                    this.renderCar(
+                      require("../assets/products/silver-car.jpg")
+                    );
+                  }}
+                  style={{ cursor: "pointer" }}
+                >
+                  <img
+                    className="col-2"
+                    src={require("../assets/icons/icon-silver.jpg")}
+                    alt="silver_icon"
+                  />
+                  <div className="col-10">
+                    <h3>Crystal Black</h3>
+                    <p>Pearl</p>
+                  </div>
                 </div>
-              </div>
-              <div className="row border border-link pt-2 pb-2 mt-2">
-                <img
-                  className="col-2"
-                  src={require("../assets/icons/icon-red.jpg")}
-                  alt="red_icon"
-                />
-                <div className="col-10">
-                  <h3>Crystal Black</h3>
-                  <p>Pearl</p>
+                <div
+                  className="row border border-link pt-2 pb-2 mt-2"
+                  onClick={() => {
+                    this.renderCar(require("../assets/products/red-car.jpg"));
+                  }}
+                  style={{ cursor: "pointer" }}
+                >
+                  <img
+                    className="col-2"
+                    src={require("../assets/icons/icon-red.jpg")}
+                    alt="red_icon"
+                  />
+                  <div className="col-10">
+                    <h3>Crystal Black</h3>
+                    <p>Pearl</p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
